314cee50ef498f7431ffd8a7aa4c8c80f5e4aa67
unknown <jreidmke@gmail.com>
2020-10-29 11:51:12 -0500

    added webCat func

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const fs = require("fs");
+const axios = require("axios");
 
-fs.readFile(process.argv[2], 'utf-8', function(err, data) {
-    if(err) {
-        console.log(err);
-        process.exit(1);
-    }
-    console.log(data);
-})
+function readLocal() {
+    fs.readFile(process.argv[2], 'utf-8', function(err, data) {
+        if(err) {
+            console.log(err);
+            process.exit(1);
+        }
+        console.log(data);
+    })
+}
+
+async function webCat() {
+    const data = (await axios.get(process.argv[2]));
+    fs.readFile(data.data, 'utf-8', (err, data) => {
+        if(err) {
+            console.log(err);
+            process.exit(1);
+        }
+        console.log(data);
+    })
+}
+
+if(process.argv[2].slice(0, 7) === "http://") {
+    webCat(process.argv[2])
+} else {
+    readLocal(process.argv[2])
+}
